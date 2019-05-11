072574d79d209a39de4cfdb53bed1cc9a995f02a
jgarn <jgarn@DESKTOP-D4BAQIP>
2019-05-10 19:35:16 -0500

    Punto 1 lab

--- src/unal/poo/practica/RobotBase.java
+++ src/unal/poo/practica/RobotBase.java
@@ -10,7 +10,8 @@ public class RobotBase
        //Declaracion de Variables -- Forma temporal - No es buena practica tener
        //variables estaticas
         public static City objetos;
-        public static Robot estudiante;
+        public static Robot Vaca;
+        public static Robot Granjero;
         
 	public static void main (String[] args){
             //Declarar la creacion de la ciudad
@@ -19,42 +20,42 @@ public class RobotBase
             
             //Direction.NORTH, EAST, SOUTH, WEST
             //Definicion de la ubicacion del robot, Ciudad, posicion, Direccion, Numero things en el bolso.
-            estudiante = new Robot(objetos,0, 2, Direction.EAST,10);
+            Vaca = new Robot(objetos,0, 2, Direction.EAST,10);
             
 	    //Mover una interseccion en el sentido al cual este apuntando el objeto.
-            estudiante.move ();
+            Vaca.move ();
             
             //Girar a la izquierda
-            estudiante.turnLeft();
+            Vaca.turnLeft();
             
             //Tomando decisiones, Si puedo tomar un Thing
-            boolean puedeTomar = estudiante.canPickThing();
+            boolean puedeTomar = Vaca.canPickThing();
             
             //Tomar un Thing
             if(puedeTomar == true)
-               estudiante.pickThing();
+               Vaca.pickThing();
             
             //Especifica el numero de Thing que tiene en robot en el bolso
-            int numeroThings = estudiante.countThingsInBackpack();
+            int numeroThings = Vaca.countThingsInBackpack();
             
             //Poner Thing, se debe validar que tenga things en el bolso
-            estudiante.putThing();
+            Vaca.putThing();
                        
             //Si el frente esta libre de Wall
-            estudiante.frontIsClear();
+            Vaca.frontIsClear();
             
             //Invocando una funcion
             creacionFuncion(4);
             
             //Toman un Thing
-            estudiante.pickThing();
+            Vaca.pickThing();
             
             
 	}
         
         public static void creacionFuncion(int parametroEntrada){
             for (int i = 0; i < parametroEntrada; i++) 
-                estudiante.move();
+                Vaca.move();
         }
 }
 
